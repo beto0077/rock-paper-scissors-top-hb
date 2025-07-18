3b84443c20ce484500459510de766a0d900f2c8c
beto0077 <131410367+beto0077@users.noreply.github.com>
2025-07-18 02:37:53 -0600

    UI implementation for the game is complete, pending evaluation for minor visual improvements.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const roundCount = document.querySelector("#round-count");
 const rpsButtons = document.querySelector(".rps-buttons");
 const choiceButtons = document.querySelectorAll(".choice-button");
 const resultsPanel = document.querySelector(".results-panel");
+const resultsTitle = document.querySelector(".results-title");
 const roundResult = document.querySelector("#round-result");
 const gameStats = document.querySelector("#game-stats");
 const finalResult = document.querySelector("#final-result");
@@ -32,14 +33,15 @@ function handleRoundFormSubmit(e) {
 
 function showCurrentRoundWinner(winner) {
     //If the value received is true, the computer won the round, if the value received is false, the user won the round.
-    let userWonRound = `The user won this round, ${userChoice} beats ${computerChoice}`;
-    let computerWonRound = `The computer won this round, ${computerChoice} beats ${userChoice}`;
+    let userWonRound = `The user won this round, ${capitalizeFirstLetter(userChoice)} beats ${capitalizeFirstLetter(computerChoice)}`;
+    let computerWonRound = `The computer won this round, ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(userChoice)}`;
     winner ? roundResult.textContent = computerWonRound : roundResult.textContent = userWonRound;
 }
 
 function compareChosenOptions(userOption, computerOption) {
+    resultsTitle.style.display = "block";
     if (userOption === computerOption) {
-        let currentRoundTied = `Both players made the same choice, so this round is a tie. User chose ${userOption} and computer chose ${computerOption}.`;
+        let currentRoundTied = `Both players made the same choice, so this round is a tie. User chose ${capitalizeFirstLetter(userOption)} and computer chose ${capitalizeFirstLetter(computerOption)}.`;
         roundResult.textContent = currentRoundTied;
         tieCount++;
     } else {
@@ -69,24 +71,23 @@ function showGameWinner(userData, computerData, tieData) {
     rpsButtons.style.display = "none";
     startButton.style.display = "block";
     gameStats.textContent = "";
-    let userWin = `The user has won the game, congratulations! \nUser score: ${userData} | Computer score: ${computerData} | Tied rounds: ${tieData}`;
-    let computerWin = `The computer has won the game, better luck next time. \nUser score: ${userData} | Computer score: ${computerData} | Tied rounds: ${tieData}`;
-    let tieEndGame = `The game ended in a tie, both players have the same score at the end of the total number of rounds played. \nUser score: ${userData} | Computer score: ${computerData} | Tied rounds: ${tieData}`;
+    let userWin = `The user has won the game, congratulations! <br>User score: ${userData} | Computer score: ${computerData} | Tied rounds: ${tieData}`;
+    let computerWin = `The computer has won the game, better luck next time. <br>User score: ${userData} | Computer score: ${computerData} | Tied rounds: ${tieData}`;
+    let tieEndGame = `The game ended in a tie, both players have the same score at the end of the total number of rounds played. <br>User score: ${userData} | Computer score: ${computerData} | Tied rounds: ${tieData}`;
     if (userData == computerData) {
         console.log(tieEndGame);
-        finalResult.textContent = tieEndGame;
+        finalResult.innerHTML = tieEndGame;
     } else if (userData > computerData) {
         console.log(userWin);
-        finalResult.textContent = userWin;
+        finalResult.innerHTML = userWin;
     } else {
         console.log(computerWin);
-        finalResult.textContent = computerWin;
+        finalResult.innerHTML = computerWin;
     }
 }
 
 function handleUserChoice(chosenElement) {
-    //The data was not being obtained correctly with "chosenElement.dataset.choice", after debugging with the console it was discovered that the correct address was this "chosenElement.target.dataset.choice".
-    userChoice = chosenElement.target.dataset.choice;
+    userChoice = chosenElement.target.offsetParent.dataset.choice;
     computerChoice = rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
     compareChosenOptions(userChoice, computerChoice);
     currentRound++;
@@ -103,6 +104,7 @@ function startNewGame() {
     userScore = 0;
     computerScore = 0;
     tieCount = 0;
+    resultsTitle.style.display = "none";
     roundResult.textContent = "";
     finalResult.textContent = "";
     startButton.style.display = "none";
